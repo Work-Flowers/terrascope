fb205acb32ac7b8ed57dae9fa5cbeab7939ec2b5
denchiuten <d.chiuten@icloud.com>
2025-02-28 10:11:01 +0800

    removed UNION ALL for placeholder rows which are no longer necessary

--- main_capex.sql
+++ main_capex.sql
@@ -74,114 +74,56 @@ WHERE
 
 UNION
 
-(
-	SELECT DISTINCT 
-		'actual_project_members' AS section,
-		-- member's details -- 
-		e.id,
-		e.full_name,
-		e.email,
-		e.department,
-		et.team_name AS team,
-		e.title,
-		map.development_share,
-		map.shared_resource,
-		INITCAP(hist.status) AS status,
-		hist.from_date,
-		
-		-- project details -- 
-		p.id AS project_id,
-		p.name AS project_name,
-		INITCAP(p.state) AS project_status,
-		DATE_TRUNC('month', p.started_at)::DATE AS project_start_date,
-		p.target_date::DATE AS project_target_date,
-		p.completed_at::DATE AS project_completed_date,
-		active_projects.issue_completion_date AS issue_completed_at,
-		active_projects.initiative_name
-	FROM google_sheets.capex_mapping AS map
-	INNER JOIN hibob.employee AS e
-		-- TRIM and LOWER for trailing white space/improper capitalisation, and JOIN on department since some job titles may exist in multiple departments 
-		ON TRIM(LOWER(map.role)) = TRIM(LOWER(e.title))
-		AND map.department = e.department
-    INNER JOIN hibob.vw_employee_team AS et
-        ON e.id = et.employee_id
-	INNER JOIN linear.users AS u
-		ON LOWER(e.email) = LOWER(u.email)
-	LEFT JOIN linear.project_member AS mem
-		ON u.id = mem.member_id
-		AND mem._fivetran_deleted IS FALSE
-	LEFT JOIN linear.project AS p
-		ON mem.project_id = p.id
-		AND p._fivetran_deleted IS FALSE
-	
-	-- INNER JOIN to include only truly active projects, with storypoints completed within the month --
-	INNER JOIN active_projects
-		ON mem.project_id = active_projects.project_id
-	INNER JOIN employee_history AS hist
-		ON e.id = hist.employee_id
-		AND hist.status = 'employed'
-		AND active_projects.issue_completion_date BETWEEN hist.from_date AND hist.to_date
 
-	-- only capitalisable resources from within CapEx Roles Mapping (filters out non capitalisable roles in case they are somehow within the sheet) --
-	-- also filters only active employees or terminated/garden leave employees who contributed during the reporting month -- 
-	WHERE 
-		1 = 1
-		AND map.development_share > 0
+SELECT DISTINCT 
+	'actual_project_members' AS section,
+	-- member's details -- 
+	e.id,
+	e.full_name,
+	e.email,
+	e.department,
+	et.team_name AS team,
+	e.title,
+	map.development_share,
+	map.shared_resource,
+	INITCAP(hist.status) AS status,
+	hist.from_date,
 	
-	UNION ALL
-	
-	-- add dummy rows for every combination of person and month
-	SELECT DISTINCT 
-		'placeholder_rows' AS section,
-		-- member's details -- 
-		e.id,
-        e.full_name,
-		e.email,
-		e.department AS department,
-		et.team_name AS team,
-		e.title AS title,
-		map.development_share,
-		map.shared_resource,
-		INITCAP(hist.status) AS status,
-		hist.from_date,
-			
-		-- project details -- 
-		NULL AS project_id,
-		NULL AS project_name,
-		NULL AS project_status,
-		DATE_TRUNC('month', p.started_at)::DATE AS project_start_date,
-		p.target_date::DATE AS project_target_date,
-		p.completed_at::DATE AS project_completed_date,
-		
-		-- dummy completed date -- 
-		DATE_TRUNC('month', d.date)::DATE AS completed_at,
-		COALESCE(ip.initiative_name, '2025 Features Roadmap') AS initiative_name
-	
-	FROM google_sheets.capex_mapping AS map
-	INNER JOIN hibob.employee AS e
-		-- add RTRIM and LOWER to correct for trailing white space and improper capitalisation --
-		ON RTRIM(LOWER(map.role)) = RTRIM(LOWER(e.title))
-		
-		-- also include department in the JOIN since some job titles may exist in multiple departments --
-		AND map.department = e.department
+	-- project details -- 
+	p.id AS project_id,
+	p.name AS project_name,
+	INITCAP(p.state) AS project_status,
+	DATE_TRUNC('month', p.started_at)::DATE AS project_start_date,
+	p.target_date::DATE AS project_target_date,
+	p.completed_at::DATE AS project_completed_date,
+	active_projects.issue_completion_date AS issue_completed_at,
+	active_projects.initiative_name
+FROM google_sheets.capex_mapping AS map
+INNER JOIN hibob.employee AS e
+	-- TRIM and LOWER for trailing white space/improper capitalisation, and JOIN on department since some job titles may exist in multiple departments 
+	ON TRIM(LOWER(map.role)) = TRIM(LOWER(e.title))
+	AND map.department = e.department
     INNER JOIN hibob.vw_employee_team AS et
         ON e.id = et.employee_id
-	INNER JOIN linear.users AS u
-		ON LOWER(e.email) = LOWER(u.email)
-	INNER JOIN linear.project_member AS mem
-		ON u.id = mem.member_id
-		AND mem._fivetran_deleted IS FALSE
-	INNER JOIN linear.project AS p
-		ON mem.project_id = p.id
-		AND p._fivetran_deleted IS FALSE
-	INNER JOIN google_sheets.initiatives_to_projects AS ip
-		ON p.id = ip.project_id
-		AND ip.initiative_id = '063b054f-7b7d-49b0-9f93-96701ee6ee9d' -- ID for 2025 Features Roadmap
-	INNER JOIN plumbing.dates AS d
-		-- dummy dates that are between the start of the year and current date --
-		ON d.date BETWEEN DATE_TRUNC('year', CURRENT_DATE) AND CURRENT_DATE
-	INNER JOIN employee_history AS hist
-		ON e.id = hist.employee_id
-		AND hist.status = 'employed'
-		AND d.date BETWEEN hist.from_date AND hist.to_date
-)
+INNER JOIN linear.users AS u
+	ON LOWER(e.email) = LOWER(u.email)
+LEFT JOIN linear.project_member AS mem
+	ON u.id = mem.member_id
+	AND mem._fivetran_deleted IS FALSE
+LEFT JOIN linear.project AS p
+	ON mem.project_id = p.id
+	AND p._fivetran_deleted IS FALSE
+
+-- INNER JOIN to include only truly active projects, with storypoints completed within the month --
+INNER JOIN active_projects
+	ON mem.project_id = active_projects.project_id
+INNER JOIN employee_history AS hist
+	ON e.id = hist.employee_id
+	AND hist.status = 'employed'
+	AND active_projects.issue_completion_date BETWEEN hist.from_date AND hist.to_date
+
+-- only capitalisable resources from within CapEx Roles Mapping (filters out non capitalisable roles in case they are somehow within the sheet) --
+-- also filters only active employees or terminated/garden leave employees who contributed during the reporting month -- 
+WHERE 
+	1 = 1
+	AND map.development_share > 0
